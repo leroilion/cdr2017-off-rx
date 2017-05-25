0500187d74c70ebea31de975e3ea81bf22e691f5
Jérémy Cheynet <jerem.cheynet@gmail.com>
2017-05-25 22:25:14 +0200

    feat: add first version of receiver code
    Signed-off-by: Jérémy Cheynet <jerem.cheynet@gmail.com>

--- main.ino
+++ main.ino
@@ -1,6 +1,9 @@
 #include <Arduino.h>
+#include <VirtualWire.h>
 
-#define LED 13
+#define RESTART_PIN 5
+
+#define BUFFER_SIZE 4
 
 void setup()
 {
@@ -9,14 +12,55 @@ void setup()
     Serial.print(F("VERSION : "));
     Serial.println(F(VERSION));
 
-    /// Init LED
-    pinMode(LED, OUTPUT);
+    pinMode(RESTART_PIN, INPUT);
+    digitalWrite(RESTART_PIN, HIGH);
+
+    vw_setup(2000);
+    vw_rx_start();
 }
 
 void loop()
 {
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
-    delay(1000);
+    static unsigned long lastUpdate = 0;
+    uint16_t power = 0;
+    uint16_t direction = 0;
+    boolean received = false;
+    boolean activateRobot = true;
+    uint8_t buf[BUFFER_SIZE];
+    uint8_t buflen = BUFFER_SIZE;
+    if (vw_wait_rx_max(100)) // Si un message est reçu dans les 100ms qui viennent
+    {
+        if (vw_get_message(buf, &buflen)) // On copie le message, qu'il soit corrompu ou non
+        {
+            power = buf[1];
+            power = power << 8;
+            power += buf[0];
+            direction = buf[3];
+            direction = (direction << 8);
+            direction += buf[2];
+            received = true;
+        }
+    }
+
+    if( millis() - lastUpdate > 500 )
+    {
+        // Stop robot
+        activateRobot = false;
+        Serial.println(F("Stop robot"));
+    }
+
+    if( activateRobot && received )
+    {
+        lastUpdate = millis();
+        Serial.println(F("Set command"));
+        Serial.print(power);
+        Serial.print(F("|"));
+        Serial.println(direction);
+    }
+
+    if( digitalRead(RESTART_PIN) == LOW )
+    {
+        Serial.println(F("Restart robot"));
+        lastUpdate = millis();
+    }
 }
